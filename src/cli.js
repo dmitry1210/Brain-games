cb61f3b802abd7996be6c9ead81c9890d2698bcf
Dmitry E <dmitry1210@gmail.com>
2022-07-27 15:56:19 +0300

    add cli.js & greeting functionality to the project

---
+++ src/cli.js
@@ -0,0 +1,13 @@
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+
+export const userName = readlineSync.question('May I have your name? ');
+
+console.log('Hi ' + userName + '!');
+
+// export const getUserName = () => {
+//     const userName = readlineSync.question('May I have your name? ');
+//     return userName;
+// };
+
