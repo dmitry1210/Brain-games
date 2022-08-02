10ed5f2eaa49deb8b038b7c938475d2588d20dd5
Dmitry E <dmitry1210@gmail.com>
2022-08-02 16:59:37 +0300

    finished calc game

--- src/calc.js
+++ src/calc.js
@@ -9,28 +9,44 @@ console.log('Hello, ' + userName + '!\nWhat is the result of this expression?');
 const maxRoundsWon = 3;
 let roundsCounter = 0;
 while (roundsCounter < maxRoundsWon) {
-    const getRandomNumber1 = () => {
+    const getRandomNumber = () => {
         return Math.round(Math.random() * 10);
     }
-    let number1 = getRandomNumber1();
+    let number1 = getRandomNumber();
+    let number2 = getRandomNumber();
 
-    const getRandomNumber2 = () => {
-        return Math.round(Math.random() * 10);
-    }
-    let number2 = getRandomNumber2();
+    const operatorsArr = ['+', '-', '*'];
+    const getRandomOperator = () => {
+        return operatorsArr[Math.floor(Math.random() * operatorsArr.length)];
+    } 
+    let operator = getRandomOperator();
 
-    console.log('Question: ' + number1 + ' + ' + number2);
-    let corretAnswer = number1 + number2;
+    console.log('Question: ' + number1 + ' ' + operator + ' ' + number2);
+
+    let corretAnswer = 1;
+    switch (operator) {
+        case '+':
+            corretAnswer = number1 + number2;
+            break;
+        case '-':
+            corretAnswer = number1 - number2;
+            break;
+        case '*':
+            corretAnswer = number1 * number2;
+            break;
+    };
 
     const userAnswer = readlineSync.question('Your answer: ');
-    
+
     if (corretAnswer == userAnswer) {
         console.log('Correct!');
         roundsCounter++;
-} else {
-    console.log('Sorry.');
-}
-}
-if (roundsCounter === maxRoundsWon) {
-    console.log('Congratulations, ' + userName + '!')
+    } else {
+        console.log('\'' + userAnswer + '\'' + ' is wrong answer ;(. Correct answer was ' + '\'' + corretAnswer + '\'.\nLet\'s try again, ' + userName + '!');
+        break;
+    }
+
+    if (roundsCounter === maxRoundsWon) {
+        console.log('Congratulations, ' + userName + '!')
+    };    
 };
