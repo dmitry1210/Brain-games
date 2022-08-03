e1cdde69f2f154612062b04669e9319ff57bac8d
Dmitry E <dmitry1210@gmail.com>
2022-08-03 13:41:42 +0300

    finished brain-calc game

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 export const greeting = () => {
     console.log('Welcome to the Brain Games!');
 
-    const userName = readlineSync.question('May I have your name? ');
+    userName = readlineSync.question('May I have your name? ');
     
     console.log('Hello, ' + userName + '!');
 };
--- src/index.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-
-import { greeting } from "cli.js";
