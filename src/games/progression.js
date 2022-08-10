898646c37fe21810df07db271617c007454ce541
Dmitry E <dmitry1210@gmail.com>
2022-08-10 11:41:16 +0300

    complete the progression game

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,53 +5,48 @@ export const userName = greeting();
 
 console.log('What number is missing in the progression?');
 
-let firstNum = getRandomNumber();
-let increment = getRandomNumber();
-let newNum = firstNum + increment;
-let randomIndex = getRandomNumber();
-let dots = '..';
+const maxRoundsWon = 3;
+let roundsCounter = 0;
+while (roundsCounter < maxRoundsWon) {
 
-const getProgression = () => {
-    let result = [];
-    result.push(firstNum);
+    let firstNum = getRandomNumber();
+    let increment = getRandomNumber();
+    let newNum = firstNum + increment;
+    let randomIndex = getRandomNumber();
+    
+    const getProgression = () => {
+        let result = [];
+        result.push(firstNum);
+    
+        for (let i = 1; i < 10; i++) {
+            result.push(newNum);
+            newNum = newNum + increment;
+        }
+    
+        return result;
+    }
+    
+    const progressionRow = getProgression();
+    
+    let correctAnswer = progressionRow[randomIndex];
+    
+    delete progressionRow[randomIndex];
+    progressionRow[randomIndex] = '..';
+    
+    console.log('Question: ' + progressionRow.join(' '));
+    // console.log('CorrectAnswerCheck: ' + correctAnswer);
 
-    for (let i = 1; i < 10; i++) {
-        result.push(newNum);
-        newNum = newNum + increment;
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (correctAnswer == userAnswer) {
+        console.log('Correct!');
+        roundsCounter++;
+    } else {
+        console.log('\'' + userAnswer + '\'' + ' is wrong answer ;(. Correct answer was ' + '\'' + correctAnswer + '\'.\nLet\'s try again, ' + userName + '!');
+        break;
     }
 
-    return result;
-}
-
-const progressionRow = getProgression();
-
-let correctAnswer = progressionRow[randomIndex];
-
-delete progressionRow[randomIndex];
-progressionRow[randomIndex] = '..';
-
-console.log('Question: ' + progressionRow);
-console.log('QuestionCheck: ' + correctAnswer);
-
-
-
-// const maxRoundsWon = 3;
-// let roundsCounter = 0;
-// while (roundsCounter < maxRoundsWon) {
-
-//     let corretAnswer = getNOD(number1, number2);
-
-//     const userAnswer = readlineSync.question('Your answer: ');
-
-//     if (corretAnswer == userAnswer) {
-//         console.log('Correct!');
-//         roundsCounter++;
-//     } else {
-//         console.log('\'' + userAnswer + '\'' + ' is wrong answer ;(. Correct answer was ' + '\'' + corretAnswer + '\'.\nLet\'s try again, ' + userName + '!');
-//         break;
-//     }
-
-//     if (roundsCounter === maxRoundsWon) {
-//         console.log('Congratulations, ' + userName + '!')
-//     };    
-// };
+    if (roundsCounter === maxRoundsWon) {
+        console.log('Congratulations, ' + userName + '!')
+    };    
+};
