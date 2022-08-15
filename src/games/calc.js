03c8819b63c994a61dee86f9a3a7d23df1cd00ed
Dmitry E <dmitry1210@gmail.com>
2022-08-15 12:05:22 +0300

    add esling settings

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,5 +1,5 @@
 import readlineSync from 'readline-sync';
-import {greeting, getRandomNumber} from '../index.js';
+import { greeting, getRandomNumber } from '../index.js';
 
 export const userName = greeting();
 
@@ -8,41 +8,42 @@ console.log('What is the result of this expression?');
 const maxRoundsWon = 3;
 let roundsCounter = 0;
 while (roundsCounter < maxRoundsWon) {
-    const number1 = getRandomNumber();
-    const number2 = getRandomNumber();
+  const number1 = getRandomNumber();
+  const number2 = getRandomNumber();
 
-    const operatorsArr = ['+', '-', '*'];
-    const getRandomOperator = () => {
-        return operatorsArr[Math.floor(Math.random() * operatorsArr.length)];
-    } 
-    let operator = getRandomOperator();
+  const operatorsArr = ['+', '-', '*'];
+  const getRandomOperator = () => operatorsArr[Math.floor(Math.random() * operatorsArr.length)];
+  const operator = getRandomOperator();
 
-    console.log('Question: ' + number1 + ' ' + operator + ' ' + number2);
+  console.log('Question: ' + number1 + ' ' + operator + ' ' + number2);
 
-    let corretAnswer = 1;
-    switch (operator) {
-        case '+':
-            corretAnswer = number1 + number2;
-            break;
-        case '-':
-            corretAnswer = number1 - number2;
-            break;
-        case '*':
-            corretAnswer = number1 * number2;
-            break;
-    };
+  let corretAnswer = 1;
+  switch (operator) {
+    case '+':
+      corretAnswer = number1 + number2;
+      break;
+    case '-':
+      corretAnswer = number1 - number2;
+      break;
+    case '*':
+      corretAnswer = number1 * number2;
+      break;
+    default:
+    // do nothing
+}
 
-    const userAnswer = readlineSync.question('Your answer: ');
+  const userAnswer = readlineSync.question('Your answer: ');
 
-    if (corretAnswer == userAnswer) {
-        console.log('Correct!');
-        roundsCounter++;
-    } else {
-        console.log('\'' + userAnswer + '\'' + ' is wrong answer ;(. Correct answer was ' + '\'' + corretAnswer + '\'.\nLet\'s try again, ' + userName + '!');
-        break;
-    }
+  if (corretAnswer == userAnswer) {
+    console.log('Correct!');
+    roundsCounter += 1;
+  } else {
 
-    if (roundsCounter === maxRoundsWon) {
-        console.log('Congratulations, ' + userName + '!')
-    };    
-};
+    console.log('\'' + userAnswer + '\'' + ' is wrong answer ;(. Correct answer was ' + '\'' + corretAnswer + '\'.\nLet\'s try again, ' + userName + '!');
+    break;
+  }
+
+  if (roundsCounter === maxRoundsWon) {
+    console.log('Congratulations, ' + userName + '!');
+  }
+}
