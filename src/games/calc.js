478ae3cdbc9e8de5d3befa589016810dd9df30d0
Dmitry E <dmitry1210@gmail.com>
2022-08-18 09:22:39 +0300

    !@#$%^&*()

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,15 +3,14 @@ import { getRandomNumber, getOperator } from '../utilities.js';
 import { greeting, askQuestion, gameRound } from '../index.js';
 
 const calcGame = () => {
-  const userName = greeting();
-
-  askQuestion('What is the result of this expression?');
+  console.log('What is the result of this expression?');
 
   const number1 = getRandomNumber();
   const number2 = getRandomNumber();
   const operator = getOperator();
 
-  askQuestion(`Question: ${number1} ${operator} ${number2}`);
+  // askQuestion(`Question: ${number1} ${operator} ${number2}`);
+  const question = `${number1} ${operator} ${number2}`;
 
   let correctAnswer;
   switch (operator) {
@@ -28,9 +27,10 @@ const calcGame = () => {
     // do nothing
   }
 
-  const userAnswer = Number(readlineSync.question('Your answer: '));
+  // const userAnswer = Number(readlineSync.question('Your answer: '));
+  // gameRound(correctAnswer, userAnswer, userName);
 
-  gameRound(correctAnswer, userAnswer, userName);
+  return [question, correctAnswer];
 };
 
 export default calcGame;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,41 @@
 import readlineSync from 'readline-sync';
+import calcGame from './games/calc';
 
 export const greeting = () => {
   console.log('Welcome to the Brain Games!');
-
   const userName = readlineSync.question('May I have your name? ');
-
   console.log(`Hello, ${userName}!`);
-
   return userName;
 };
 
-export const askQuestion = (question) => {
-  console.log(`${question}`);
-};
-export const gameRound = (correctAnswer, userAnswer, userName) => {
+// export const askQuestion = (question) => {
+//   console.log(`${question}`);
+// };
+
+export const gameRound = (roundData) => {
   const maxRoundsWon = 3;
   let roundsCounter = 0;
+  const userName = readlineSync.question('May I have your name? ');
   while (roundsCounter < maxRoundsWon) {
+    const [question, correctAnswer] = roundData();
+
+    console.log(`Question: ${question}`);
+    const userAnswer = Number(readlineSync.question('Your answer: '));
+
     if (correctAnswer === userAnswer) {
       console.log('Correct!');
       roundsCounter += 1;
-    } else if (roundsCounter === maxRoundsWon) {
-      console.log(`Congratulations, ${userName}!`);
     } else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
       break;
     }
   }
+  if (roundsCounter === maxRoundsWon) {
+    console.log(`Congratulations, ${userName}!`);
+  }
 };
+
+console.log(gameRound(calcGame));
 
 export function getNOD(x, y) {
   if ((typeof x !== 'number') || (typeof y !== 'number')) {
