32fdffd9b41ecf209a94f5260f3d64ace913cd95
Dmitry E <dmitry1210@gmail.com>
2022-09-12 14:11:44 +0300

    add some optimization to calc & even games

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,18 +3,13 @@ import { startGameRound } from '../index.js';
 
 const gameRules = 'What is the result of this expression?';
 const startCalcGame = () => {
-  // const roundData = [];
-  const getRoundData = () => {
-    const number1 = getRandomNumber();
-    const number2 = getRandomNumber();
-    const operator = getOperator();
-    const question = `${number1} ${operator} ${number2}`;
-    const correctAnswer = getExpressionResult(number1, operator, number2);
+  const number1 = getRandomNumber();
+  const number2 = getRandomNumber();
+  const operator = getOperator();
+  const question = `${number1} ${operator} ${number2}`;
+  const correctAnswer = getExpressionResult(number1, operator, number2);
 
-    // roundData.push(question, correctAnswer);
-    return [question, correctAnswer];
-  };
-  startGameRound(getRoundData(), gameRules);
+  return [question, correctAnswer];
 };
 
-export default startCalcGame;
+export default () => startGameRound(startCalcGame, gameRules);
--- src/games/even.js
+++ src/games/even.js
@@ -1,22 +1,22 @@
 import { getRandomNumber, isEven } from '../utilities.js';
-import { roundsNumber, startGameRound } from '../index.js';
+import { startGameRound } from '../index.js';
 
 const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
 const startEvenGame = () => {
-  let roundsCounter = 0;
-  const roundData = [];
-  const getRoundData = () => {
-    while (roundsCounter < roundsNumber) {
-      const number = getRandomNumber();
-      const question = `${number}`;
-      const correctAnswer = isEven(number) ? 'yes' : 'no';
+  // let roundsCounter = 0;
+  // const roundData = [];
+  // const getRoundData = () => {
+  // while (roundsCounter < roundsNumber) {
+  const number = getRandomNumber();
+  const question = `${number}`;
+  const correctAnswer = isEven(number) ? 'yes' : 'no';
 
-      roundData.push([question, correctAnswer]);
-      roundsCounter += 1;
-    }
-    return roundData;
-  };
-  startGameRound(getRoundData(), gameRules);
+  //   roundData.push([question, correctAnswer]);
+  //   roundsCounter += 1;
+  // }
+  return [question, correctAnswer];
+  // };
+  // startGameRound(getRoundData(), gameRules);
 };
 
-export default startEvenGame;
+export default () => startGameRound(startEvenGame, gameRules);
